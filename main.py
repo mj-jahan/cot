dd723a1d097eef2d5dd52ca70b142e9fefbfd33b
Jawad <jawad.jahan@proton.me>
2022-11-10 22:32:48 +0000

    refactored main.py for now. time to work on plotting functionality

--- main.py
+++ main.py
@@ -13,39 +13,40 @@ import numpy as np
 
 path = "raw_data"
 
-date_col = 'Report_Date_as_MM_DD_YYYY'
-
 fields = ['Market_and_Exchange_Names',
           'Report_Date_as_MM_DD_YYYY',
           'Pct_of_OI_Tot_Rept_Long_All',
           'Pct_of_OI_Tot_Rept_Short_All']
 
-
 CFTC_CAD = "CANADIAN DOLLAR - CHICAGO MERCANTILE EXCHANGE"
 year = 2020
-
-data_2020 = pd.read_excel(f'{path}/{year}.xls', index_col=0, usecols=fields, dayfirst=True)
-
-data_2020 = data_2020.rename(columns={
-    'Market_and_Exchange_Names': 'Market',
-    'Report_Date_as_MM_DD_YYYY': 'Date',
-    'Pct_of_OI_Tot_Rept_Long_All': 'OI_LONG',
-    'Pct_of_OI_Tot_Rept_Short_All': 'OI_SHORT'})
-
-cadLen = len(data_2020.loc[CFTC_CAD]['Date'])  # length of dataset to work with later
 
 # variables to obtain from filtered data set
 cad_date = []
 cad_oi_long = []
 cad_oi_short = []
-
-# iterate sample and store data in local
-for index, row in data_2020.loc[CFTC_CAD].iterrows():
-    cad_date.append(row.loc["Date"])
-    cad_oi_long.append(row.loc["OI_LONG"])
-    cad_oi_short.append(row.loc["OI_SHORT"])
-
 columns = ["Date", "OI_LONG", "OI_SHORT"]
-CAD_DATA = pd.DataFrame(list(zip(cad_date, cad_oi_long, cad_oi_short)), columns=columns)
 
-print(CAD_DATA)
+
+def getAssetSpecificCOT():
+    cot_specific = pd.read_excel(f'{path}/{year}.xls', index_col=0, usecols=fields, dayfirst=True)
+
+    cot_specific = cot_specific.rename(columns={
+        'Market_and_Exchange_Names': 'Market',
+        'Report_Date_as_MM_DD_YYYY': 'Date',
+        'Pct_of_OI_Tot_Rept_Long_All': 'OI_LONG',
+        'Pct_of_OI_Tot_Rept_Short_All': 'OI_SHORT'})
+
+    cadLen = len(cot_specific.loc[CFTC_CAD]['Date'])  # length of dataset to work with later
+
+    # iterate sample and store data in local
+    for index, row in cot_specific.loc[CFTC_CAD].iterrows():
+        cad_date.append(row.loc["Date"])
+        cad_oi_long.append(row.loc["OI_LONG"])
+        cad_oi_short.append(row.loc["OI_SHORT"])
+
+    CAD_DATA = pd.DataFrame(list(zip(cad_date, cad_oi_long, cad_oi_short)), columns=columns)
+    return CAD_DATA
+
+
+print(getAssetSpecificCOT())
